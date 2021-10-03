4074c99b28b6e9147e3ddda8411267ac0cb07fde
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2021-10-04 02:54:20 +0530

    9. Transfer token mechanism with transfer event

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,20 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+import "openzeppelin-solidity/contracts/utils/math/SafeMath.sol";
+
 contract Token {
-  string public constant name = "Master Token";
-  string public constant symbol = "MT";
-  uint256 public constant decimals = 18;
-  uint256 public totalSupply;
-  uint256 private totalToken = 10000;
+    using SafeMath for uint256;
 
-  // Track balances
-  mapping(address => uint256) public balanceOf;
+    // Variables
+    string public constant name = "Master Token";
+    string public constant symbol = "MT";
+    uint256 public constant decimals = 18;
+    uint256 public totalSupply;
+    uint256 private totalToken = 10000;
+    mapping(address => uint256) public balanceOf;
 
-  constructor() public {
-    totalSupply = totalToken * (10 ** decimals);
+    // Events
+    event Transfer(address indexed from, address indexed to, uint256 value);
     
-    // Assign total supply to the owner msg.sender = owner address
-    balanceOf[msg.sender] = totalSupply;
-  }
+    constructor() {
+        totalSupply = totalToken * (10 ** decimals);
+        
+        // Assign total supply to the owner msg.sender = owner address
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    // Transfer token from sender to receiver
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
 }

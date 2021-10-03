40d9c52107abc0e417a718db05d994069a5a4308
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2021-10-03 17:17:16 +0530

    7. Track & assign the total supply to the owner or deployer in contract

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -6,8 +6,15 @@ contract Token {
   string public constant symbol = "MT";
   uint256 public constant decimals = 18;
   uint256 public totalSupply;
-  uint256 public totalToken = 10000;
+  uint256 private totalToken = 10000;
+
+  // Track balances
+  mapping(address => uint256) public balanceOf;
+
   constructor() public {
     totalSupply = totalToken * (10 ** decimals);
+    
+    // Assign total supply to the owner msg.sender = owner address
+    balanceOf[msg.sender] = totalSupply;
   }
 }

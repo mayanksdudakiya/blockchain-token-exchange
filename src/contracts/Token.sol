c718fe745b8acf559f2ae4fc98f1727ff480cd78
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2021-10-03 16:21:49 +0530

    6. Contract info added like name, symbol, decimals & total supply

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -2,6 +2,11 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Token {
+  string public constant name = "Master Token";
+  string public constant symbol = "MT";
+  uint256 public constant decimals = 18;
+  uint256 private totalSupply;
   constructor() public {
+    totalSupply = 10000 * (10 ** decimals);
   }
 }

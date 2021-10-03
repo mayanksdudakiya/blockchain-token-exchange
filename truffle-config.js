6fcd87f060c357825525d99b0b157d64dfd37ae7
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2021-10-03 15:10:11 +0530

    4. Moved contracts to src folder, truffle configurations

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,7 @@
+require('babel-register');
+require('babel-polyfill');
+require('dotenv').config();
+
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -41,11 +45,11 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -72,6 +76,10 @@ module.exports = {
     // production: true    // Treats this network as if it was a public net. (default: false)
     // }
   },
+
+  // Run the contracts from src folder
+  contracts_directory: './src/contracts',
+  contracts_build_directory: './src/abis',
 
   // Set default mocha options here, use special reporters etc.
   mocha: {
